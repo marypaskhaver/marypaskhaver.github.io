4ff7742fb483b9a9cac5c056b465460d80f4fdd4
marypaskhaver <mary.paskhaver@gmail.com>
2021-10-14 21:44:02 -0400

    Add func to cycle through things I am, display in index.html

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function () {
+    const words = ["programmer", "writer", "go-getter", "teacher"];
+    var index = 0;
+
+    setInterval(function() {
+        $("#thing-i-am").fadeOut(500, function() {
+            $(this).text(words[index] + ".").fadeIn(500);
+            index += 1;
+        });
+
+        if (index == words.length) {
+            index = 0;
+        }
+    }, 2000);
+});
